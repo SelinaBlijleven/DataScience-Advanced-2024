e5b433b0eeb97d2aef7fbd91e1cccdc23c8641f8
Lina Blijleven <lina.blijleven@hu.nl>
2024-05-01 13:52:10 +0200

    Dataset actually loaded as a dataframe

--- data/import_auto_mpg.py
+++ data/import_auto_mpg.py
@@ -4,6 +4,7 @@ import_auto_mpg
 Use the following code to import the Auto MPG dataset that is missing from the files.
 Note: this does not trigger a download, but allows you to use the dataset directly from your script.
 """
+import pandas as pd
 from datasets import load_dataset
 
-dataset = load_dataset("scikit-learn/auto-mpg")
+dataset: pd.DataFrame = pd.DataFrame(load_dataset("scikit-learn/auto-mpg")['train'])
